dc1256cd2c65f3742f52401a43c150beba44efe4
Ubuntu <ubuntu@ip-172-31-29-61.us-east-2.compute.internal>
2024-01-10 14:44:11 +0000

    variable.tf is added

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "my_terra_instance"{
                         
-                    ami  = "ami-05fb0b8c1424f266b"
+                    ami  = var.mera_ec2_ami
                     instance_type = "t2.micro"
-                    key_name = "mera-server"
+                    key_name = aws_key_pair.key.key_name
           
                     tags={
                           Name = "tf_instance"
@@ -16,7 +16,13 @@ resource "aws_instance" "my_terra_instance"{
 resource "aws_s3_bucket" "meribucket"{
                   bucket = "meribuckethain"
 }
+resource "aws_key_pair" "key"{
+                    
+                  public_key = file( "/home/ubuntu/.ssh/id.pub")
+                  key_name = "mera-key"
+}
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.my_terra_instance.public_ip
 }
+
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "mera_ec2_ami"{
+                             type = string
+                             default = "ami-0cd3c7f72edd5b06d"
+}
